9a285a85da0c0985e72fe4925395346ad5de2621
johnmerm <johnmerm@gmail.com>
2014-01-15 23:33:38 +0200

    .

--- src/main/java/bioinfo/w9/SuffixArray.java
+++ src/main/java/bioinfo/w9/SuffixArray.java
@@ -11,6 +11,7 @@ import java.util.TreeSet;
 import org.apache.commons.io.IOUtils;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.Ordering;
 
 public class SuffixArray {
 	private String text;
@@ -50,22 +51,48 @@ public class SuffixArray {
 	}
 	
 	
+	public String lcr(){
+		int max = Ordering.natural().max(lcp);
+		int idx = lcp.indexOf(max);
+		int s_idx = suffixArray.get(idx);
+		return text.substring(s_idx,s_idx+max);
+	}
+	
+	
+	public List<List<Integer>> breakdown(){
+		List<List<Integer>> br = new ArrayList<List<Integer>>();
+		br.add(new ArrayList<Integer>());
+		
+		for (int i=1;i<text.length();i++){
+			
+		}
+		return br;
+	}
+	
 	public static void main(String[] args) throws Exception{
 		SuffixArray array = new SuffixArray();
 		
 		
-		InputStream f = SuffixArray.class.getResourceAsStream("dataset_96_3.txt");
-		//final String text = IOUtils.toString(f).trim();
+		InputStream f = SuffixArray.class.getResourceAsStream("dataset_95_5.txt");
+		//final String text = IOUtils.toString(f).trim()+"$";
 		
-		final String text = "AACGATAGCGGTAGA$";
+		//final String text = "AACGATAGCGGTAGA$";
+		//final String text = "ATATCGTTTTATCGTT$";
+		
+		//final String text="TCGGTAGATTGCGCCCACTC$AGGGGCTCGCAGTGTAAGAA#";
+		
+		String[] texts = IOUtils.toString(f).split("\n");
+		String text = texts[0]+"$"+texts[1]+"#";
+		
 		array.calculateSuffixArray(text);
 		array.calculateLCP();
+				
+//		for (int i=0;i<text.length();i++) {
+//			System.out.println(array.suffixArray.get(i)+"\t"+array.lcp.get(i)+"\t"+text.substring(array.suffixArray.get(i)));
+//		}
 		
-		
-		
-		for (int i=0;i<text.length();i++) {
-			System.out.println(array.suffixArray.get(i)+"\t"+array.lcp.get(i)+"\t"+text.substring(array.suffixArray.get(i)));
-		}
+		String lcr = array.lcr();
+		System.out.println(lcr);
 		
 		
 	}
